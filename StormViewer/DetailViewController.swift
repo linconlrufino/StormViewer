77600d34acb5c703652f8a7b4faa1319310d034b
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-07-16 22:31:21 -0300

    Loading images with UIImage

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -9,22 +9,14 @@ import UIKit
 
 class DetailViewController: UIViewController {
     @IBOutlet var imageVIEW: UIImageView!
+    var selectImage: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let imageToLoad = selectImage {
+            imageVIEW.image = UIImage(named: imageToLoad)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
